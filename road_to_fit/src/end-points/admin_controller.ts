0c3fa2508eb2c86ad2cdae6d330b2f29e3deb089
vnaykvkrm <vinnuvikramvin@gmail.com>
2024-11-27 23:41:21 +0530

    updated response for create admin

--- road_to_fit/src/end-points/admin_controller.ts
+++ road_to_fit/src/end-points/admin_controller.ts
@@ -18,7 +18,6 @@ app.get('/', async (c) => {
 // Create a new admin
 app.post('/create', async (c) => {
   const { name, email, password, role } = await c.req.json()
-  console.log({ name, email, password, role }, 'admin data')
 
   if (!name || !email || !password || !role) {
     return c.json({ error: 'All fields are required' }, 400)
@@ -30,7 +29,7 @@ app.post('/create', async (c) => {
 
   try {
     await createAdmin({ name, email, password, role })
-    return c.json({ message: 'Admin created successfully' }, 200)
+    return c.json({ message: `Admin ${email} created successfully` }, 200)
   } catch (error) {
     console.error(error)
     return c.json({ error: 'Failed to create admin' }, 500)
@@ -38,7 +37,7 @@ app.post('/create', async (c) => {
 })
 
 // Delete an admin
-app.delete('/admin/delete', async (c) => {
+app.delete('/delete', async (c) => {
   const { email } = await c.req.json()
   if (!email) {
     return c.json({ error: 'Email is required' }, 400)
